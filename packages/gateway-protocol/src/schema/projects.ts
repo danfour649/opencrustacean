IlyaKuprov/openclaw

feat(gateway): projects.list groups known checkouts by repo identity
Implements docs/plan/runners.md milestone 4 derived projects read model.

packages/gateway-protocol/src/schema/projects.ts
@@ -1,0 +1,34 @@
+import { Type, type Static } from "typebox";
+import { closedObject } from "./closed-object.js";
+import { NonEmptyString } from "./primitives.js";
+
+export const PROJECTS_LIST_DEFAULT_LIMIT = 50;
+export const PROJECTS_LIST_MAX_LIMIT = 200;
+
+/** One gateway-visible checkout for a derived repository project. */
+export const ProjectCheckoutSchema = closedObject({
+  runnerId: NonEmptyString,
+  path: NonEmptyString,
+});
+
+/** Repository identity derived from observed checkout and session state. */
+export const ProjectSummarySchema = closedObject({
+  name: NonEmptyString,
+  originUrl: Type.Optional(NonEmptyString),
+  checkouts: Type.Array(ProjectCheckoutSchema, { minItems: 1 }),
+  lastUsedAt: Type.Number({ minimum: 0 }),
+});
+
+/** Bounded request for derived projects, newest first. */
+export const ProjectsListParamsSchema = closedObject({
+  limit: Type.Optional(Type.Integer({ minimum: 1, maximum: PROJECTS_LIST_MAX_LIMIT })),
+});
+
+export const ProjectsListResultSchema = closedObject({
+  projects: Type.Array(ProjectSummarySchema, { maxItems: PROJECTS_LIST_MAX_LIMIT }),
+});
+
+export type ProjectCheckout = Static<typeof ProjectCheckoutSchema>;
+export type ProjectSummary = Static<typeof ProjectSummarySchema>;
+export type ProjectsListParams = Static<typeof ProjectsListParamsSchema>;
+export type ProjectsListResult = Static<typeof ProjectsListResultSchema>;

danfour649/opencrustacean

packages/gateway-protocol/src/schema/projects.ts
@@ -7,6 +7,11 @@ const StoredProjectIdSchema = Type.String({
   pattern: "^[a-z0-9][a-z0-9-]{0,63}$",
 });
 
+export const PROJECTS_LIST_DEFAULT_LIMIT = 50;
+export const PROJECTS_LIST_MAX_LIMIT = 200;
+export const PROJECTS_LIST_MAX_CHECKOUTS_PER_PROJECT = 50;
+export const PROJECTS_LIST_MAX_IDENTITY_PROBES = 32;
+
 export const ProjectRecordSchema = closedObject({
   id: NonEmptyString,
   displayName: NonEmptyString,
@@ -26,9 +31,29 @@ export const ProjectRecordSchema = closedObject({
   agentId: Type.Optional(NonEmptyString),
 });
 
-export const ProjectsListParamsSchema = closedObject({});
+/** One gateway-visible checkout for an observed repository project. */
+export const ProjectCheckoutSchema = closedObject({
+  runnerId: NonEmptyString,
+  path: NonEmptyString,
+});
+
+/** Repository identity derived from visible checkout and session state. */
+export const ProjectSummarySchema = closedObject({
+  name: NonEmptyString,
+  originUrl: Type.Optional(NonEmptyString),
+  checkouts: Type.Array(ProjectCheckoutSchema, {
+    minItems: 1,
+    maxItems: PROJECTS_LIST_MAX_CHECKOUTS_PER_PROJECT,
+  }),
+  lastUsedAt: Type.Number({ minimum: 0 }),
+});
+
+export const ProjectsListParamsSchema = closedObject({
+  limit: Type.Optional(Type.Integer({ minimum: 1, maximum: PROJECTS_LIST_MAX_LIMIT })),
+});
 export const ProjectsListResultSchema = closedObject({
-  projects: Type.Array(ProjectRecordSchema),
+  projects: Type.Array(ProjectRecordSchema, { maxItems: PROJECTS_LIST_MAX_LIMIT }),
+  observedProjects: Type.Array(ProjectSummarySchema, { maxItems: PROJECTS_LIST_MAX_LIMIT }),
 });
 
 export const ProjectsRegisterParamsSchema = closedObject({
@@ -41,6 +66,8 @@ export const ProjectsRemoveParamsSchema = closedObject({ id: StoredProjectIdSche
 export const ProjectsRemoveResultSchema = closedObject({ removed: Type.Boolean() });
 
 export type ProjectRecord = Static<typeof ProjectRecordSchema>;
+export type ProjectCheckout = Static<typeof ProjectCheckoutSchema>;
+export type ProjectSummary = Static<typeof ProjectSummarySchema>;
 export type ProjectsListParams = Static<typeof ProjectsListParamsSchema>;
 export type ProjectsListResult = Static<typeof ProjectsListResultSchema>;
 export type ProjectsRegisterParams = Static<typeof ProjectsRegisterParamsSchema>;